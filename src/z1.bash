b4236936b198ec289128b1266bbf9c4d0168321c
vdun <vdun@users.noreply.github.com>
2016-06-05 02:57:02 +0200

    Make line as comment

--- src/z1.bash
+++ src/z1.bash
@@ -13,7 +13,7 @@ z() {
 zz() {
   cd "$(_z -l 2>&1 | sed -n 's/^[ 0-9.,]*//p' | fzf -q $_last_z_args)"
 }
-Since z is not very optimal located on a qwerty keyboard I have these aliased as j and jj
+# Since z is not very optimal located on a qwerty keyboard I have these aliased as j and jj
 
 alias j=z
 alias jj=zz
